77822744d208928ab01136493e48534ae0458118
tudaoanh <tuda62@wru.vn>
2020-03-28 08:15:20 +0700

    Updates

---
+++ precache-manifest.91020576e4bb58e72ce7b5944ba9dc38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fafedaae75374287a1fe53f17aa658f",
+    "url": "/my-project1/index.html"
+  },
+  {
+    "revision": "059a251e9a38fdb10142",
+    "url": "/my-project1/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "059a251e9a38fdb10142",
+    "url": "/my-project1/static/js/2.db41b2c8.chunk.js"
+  },
+  {
+    "revision": "c1518936e7675e7d17ca66bc9ecd7e60",
+    "url": "/my-project1/static/js/2.db41b2c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "969621f502b409dca256",
+    "url": "/my-project1/static/js/main.08c3a3c5.chunk.js"
+  },
+  {
+    "revision": "cf42b7d6b18e88b187a3",
+    "url": "/my-project1/static/js/runtime-main.24c8a3fc.js"
+  },
+  {
+    "revision": "e0e27600dbadf5ce87aac430484b9f0d",
+    "url": "/my-project1/static/media/corgi.e0e27600.jpg"
+  },
+  {
+    "revision": "3e8039242e517ee7edc05a4e226e0b80",
+    "url": "/my-project1/static/media/loading.3e803924.gif"
+  }
+]);
